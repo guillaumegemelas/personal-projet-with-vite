c92d7b9ef1ff5735c8f06929adcf58e9a513c0d9
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-04-01 14:58:17 +0200

    first commit1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // Le projet a été intégralement nettoyé. il faudra recréer les fichiers indexe.css et App.css
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 export default function App() {
   return <div>Hello world gars</div>;
 }
